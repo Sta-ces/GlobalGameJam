230f4717631f3857bbdee777845ec683d8da252c
Sta-ces <staescedric@gmail.com>
2018-01-27 15:18:23 +0100

    On click object rotate

---
+++ Assets/MapGenerator/GenerateMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour {
+
+    public GameObject m_Floor;
+    public Vector3 m_MapSize;
+    public string m_ParentMapName = "GeneratedMap";
+
+    [Range(0,1)]
+    public float m_OutlinePercent;
+
+    public bool m_ChangeOnScene = true;
+
+    public void MapGenerator()
+    {
+        if (GameObject.Find(m_ParentMapName))
+        {
+            DestroyImmediate(GameObject.Find(m_ParentMapName).gameObject);
+        }
+
+        Transform mapParent = new GameObject(m_ParentMapName).transform;
+
+        for(int x = 0; x < m_MapSize.x; x++)
+        {
+            for(int y = 0; y < m_MapSize.y; y++)
+            {
+                for (int z = 0; z < m_MapSize.z; z++)
+                {
+                    Vector3 tilePosition = new Vector3(-m_MapSize.x / 2 + 0.5f + x, -m_MapSize.y / 2 + 0.5f + y, -m_MapSize.z / 2 + 0.5f + z);
+                    GameObject newTile = Instantiate(m_Floor, tilePosition, Quaternion.identity, mapParent);
+                    newTile.transform.localScale = Vector3.one * (1 - m_OutlinePercent);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scritps/RotatePipeOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RotatePipeOnClick : MonoBehaviour {
+
+    public float m_angleOfRotation = 90f;
+
+    public void OnMouseDown()
+    {
+        gameObject.transform.Rotate(transform.rotation.x, m_angleOfRotation, transform.rotation.z);
+    }
+}
